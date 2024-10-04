e6c3938ecf99e9ccd28c68b54e9c77059d73914b
Krutik Naina <nenakrutik2004@gmail.com>
2024-10-04 11:05:44 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
     $Number = $_POST["number"];
     $Email = $_POST["email"];
     $Service = $_POST["service"];
-    $Comment = $_POST["comment"];
+    $Comment = $_POST["comment"]; 
     $conn = mysqli_connect("localhost", "root", "", "pestcontrol") or die("connection failed");
     $sql = "INSERT INTO contact(name, number, email, service, comment) VALUES ('{$Name}','{$Number}','{$Email}','{$Service}','{$Comment}')";
     $result = mysqli_query($conn, $sql) or die("Query Failed!");
